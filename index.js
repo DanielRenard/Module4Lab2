efe1783f693b08b40dd945d362f7f95b957ae677
DanielRenard <danieljuderenard@gmail.com>
2023-11-15 11:34:18 -0600

    completed the main parts of lab2

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-function addCard(title, text) {
+function addCard(title = "Untitled", text = "Missing text", img = "Image not available") {
   const template = document.getElementById("card-template").content.cloneNode(true);
   template.querySelector(".card-title").innerText = title;
   template.querySelector(".card-text").innerText = text;
+  template.querySelector(".card-img").innerText = img;
   document.querySelector("#card-list").appendChild(template);
 }
+
+const data = [
+  { name: "bob", age: 23 },
+  { name: "alice", age: 39 },
+  { name: "tim", age: 56 },
+]; 
+
+const artist = {
+  name: "Van Gogh",
+  portfolio: [
+    {
+      title: "portrait",
+      url: "https://collectionapi.metmuseum.org/api/collection/v1/iiif/436532/1671316/main-image",
+    },
+    {
+      title: "sky",
+      url: "https://mymodernmet.com/wp/wp-content/uploads/2020/11/White-house-night-van-goh-worldwide-2.jpg",
+    },
+  ],
+};
+
 addCard();
-addCard();
-
-// addCard("First title", "cool beans");
-// addCard("Second title", "cooler beans");
-// addCard("Third title", "coolest beans");
-
-// const data = [
-//   { name: "bob", age: 23 },
-//   { name: "alice", age: 39 },
-// ];
-
-// const artist = {
-//   name: "Van Gogh",
-//   portfolio: [
-//     {
-//       title: "portrait",
-//       url: "https://collectionapi.metmuseum.org/api/collection/v1/iiif/436532/1671316/main-image",
-//     },
-//     {
-//       title: "sky",
-//       url: "https://mymodernmet.com/wp/wp-content/uploads/2020/11/White-house-night-van-goh-worldwide-2.jpg",
-//     },
-//   ],
-// };
+addCard("First title", "cool beans");
+addCard("Second title", "cooler beans");
+addCard("Third title", "coolest beans");
+addCard(data[0].name, data[0].age);
+addCard(data[1].name, data[1].age);
+addCard(data[2].name, data[2].age);
+addCard(artist.name, artist.portfolio[0].title, artist.portfolio[0].url);
+addCard(artist.name, artist.portfolio[1].title, artist.portfolio[1].url);
