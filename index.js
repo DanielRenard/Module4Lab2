f6109a9c0337ce390f58697783917d798f2892d8
DanielRenard <danieljuderenard@gmail.com>
2023-11-16 13:40:35 -0600

    Passed content for the card dynamically, Call addCard repeatedly, generated artist profile with image from links. need to work on extension.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function addCard(title = "Untitled", text = "Missing text", img = "Image not available") {
-  const template = document.getElementById("card-template").content.cloneNode(true);
+function addCard(title = "Untitled", text = "Missing text", url = "No Image") {
+  const template = document
+    .getElementById("card-template")
+    .content.cloneNode(true);
   template.querySelector(".card-title").innerText = title;
   template.querySelector(".card-text").innerText = text;
-  template.querySelector(".card-img").innerText = img;
+  template.querySelector(".card-img").src = url;
   document.querySelector("#card-list").appendChild(template);
 }
 
@@ -10,9 +12,9 @@ const data = [
   { name: "bob", age: 23 },
   { name: "alice", age: 39 },
   { name: "tim", age: 56 },
-]; 
+];
 
-const artist = {
+const artists = {
   name: "Van Gogh",
   portfolio: [
     {
@@ -26,12 +28,12 @@ const artist = {
   ],
 };
 
-addCard();
-addCard("First title", "cool beans");
-addCard("Second title", "cooler beans");
-addCard("Third title", "coolest beans");
-addCard(data[0].name, data[0].age);
-addCard(data[1].name, data[1].age);
-addCard(data[2].name, data[2].age);
-addCard(artist.name, artist.portfolio[0].title, artist.portfolio[0].url);
-addCard(artist.name, artist.portfolio[1].title, artist.portfolio[1].url);
+// addCard();
+// addCard("First title", "cool beans");
+// addCard("Second title", "cooler beans");
+// addCard("Third title", "coolest beans");
+// addCard(data[0].name, data[0].age);
+// addCard(data[1].name, data[1].age);
+// addCard(data[2].name, data[2].age);
+addCard(artists.name, artists.portfolio[0].title, artists.portfolio[0].url);
+addCard(artists.name, artists.portfolio[1].title, artists.portfolio[1].url);
